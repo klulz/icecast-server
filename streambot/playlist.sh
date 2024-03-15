d79a9e171607eaa12e4c22b7c64451403ba2ba13
llolo <radio@llolo.lol>
2024-03-14 17:29:57 -0700

    update to rocky linux 9

--- streambot/playlist.sh
+++ streambot/playlist.sh
@@ -3,16 +3,16 @@
 # This is a wrapper around s3playlist.py
 
 if [[ -z "${1}" ]]; then
-    # get filename to play
-    /usr/bin/env python3.8 /home/streambot/.poetry/bin/poetry run ./s3playlist.py
+	# get filename to play
+	/home/streambot/.local/bin/poetry run ./s3playlist.py
 elif [[ x"${1}" -eq "xartist" ]]; then
-    # return artist
-    # not implemented...
-    echo "not implemented" >2
+	# return artist
+	# not implemented...
+	echo "not implemented" >2
 elif [[ x"${1}" -eq "xtitle" ]]; then
-    # return artist
-    # not implemented...
-    echo "not implemented" >2
+	# return artist
+	# not implemented...
+	echo "not implemented" >2
 else
-    echo "No idea what you want from me"
+	echo "No idea what you want from me"
 fi
